cfd4cc49ac6d8d6fc9643801c12fb324298da2d7
palani001 <palanisamy@navadhiti.com>
2024-03-27 09:16:48 +0530

    basic react with vite

---
+++ mar-15/first_react/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Cards from "./card/Card";
+function App() {
+  return (
+    <>
+      <div>
+        <Cards />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ mar-15/first_react/src/card/Card.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Card.css";
+
+function Cards() {
+  return (
+    <>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <div className="box">The sample text</div>
+        <div className="box">The sample text</div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
